15aa0e09784d3a82d9ba3213e8515764f7882cf5
Kevin Hines <kevhines@gmail.com>
2021-08-16 14:28:44 -0400

    corrected some files, added cors gem

--- backend-gibberish/db/schema.rb
+++ backend-gibberish/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_182850) do
+ActiveRecord::Schema.define(version: 2021_08_16_181926) do
 
   create_table "cards", force: :cascade do |t|
     t.string "name"
