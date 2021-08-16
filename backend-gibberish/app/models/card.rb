e2719fe7853dcdd32b0522baf1539ee8d0c497cd
Kevin Hines <kevhines@gmail.com>
2021-08-16 14:04:41 -0400

    relationships added to modesl

--- backend-gibberish/app/models/card.rb
+++ backend-gibberish/app/models/card.rb
@@ -1,2 +1,7 @@
 class Card < ApplicationRecord
+    has_many :rules_winner, foreign_key: :winner_id, class_name: "Rule"
+    has_many :losers, through :rules_winner
+    
+    has_many :rules_loser, foreign_key: :loser_id, class_name: "Rule"
+    has_many :winners, though :rules_loser
 end
--- backend-gibberish/app/models/rule.rb
+++ backend-gibberish/app/models/rule.rb
@@ -1,2 +1,4 @@
 class Rule < ApplicationRecord
+    belongs_to :winner, class_name: "Card"
+    belongs_to :loser, class_name: "Card"
 end
